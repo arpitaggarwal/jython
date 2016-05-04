832803e5fcc73e854d24c46d206bb1f2a225f806
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2016-05-04 21:10:28 +0400

    regex example

--- com/jython/Functions.py
+++ com/jython/Functions.py
@@ -1,3 +1,4 @@
+import re
 def add(a,b):
     return a+b
 
@@ -7,3 +8,21 @@ def addFixedValue(a):
   
 print add(1,2)
 print addFixedValue(1)
+
+def dictionaryCheck():
+    dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
+    print "dict['Name']: ", dict['Name'] 
+    print "dict['Age']: ", dict['Age']
+   
+    
+dictionaryCheck()
+
+string = 'JBAS'
+stringnew = 'WFLYCTL0030'
+def regexCheck():
+    if re.search("WFLY[A-Z0-9]+|JBAS[0-9]+", string):
+        print"Regex : %s" % re.search("WFLY[A-Z0-9]+|JBAS[0-9]+", string)
+    else:
+        print"False " 
+    
+regexCheck()
